ed5fa904fdeb4561946e76512f7e8a79d24e7f69
rskiver89 <108497515+rskiver89@users.noreply.github.com>
2023-05-02 20:18:42 -0400

    update

--- scripts.js
+++ scripts.js
@@ -9,3 +9,43 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
   });
   
+// Project Scroll Animation
+document.addEventListener('DOMContentLoaded', function () {
+    const projectItems = document.querySelectorAll('.project-list li');
+  
+    // Function to handle the animation
+    const animateProjects = () => {
+      anime({
+        targets: projectItems,
+        opacity: [0, 1],
+        translateY: [-50, 0],
+        delay: anime.stagger(100),
+        easing: 'easeOutQuad',
+        duration: 1000
+      });
+    };
+  
+    // Intersection Observer Options
+    const options = {
+      root: null,
+      rootMargin: '0px',
+      threshold: 0.25
+    };
+  
+    // Intersection Observer Callback
+    const callback = (entries, observer) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          animateProjects();
+          observer.unobserve(entry.target); // Unobserve the target after the animation has been triggered
+        }
+      });
+    };
+  
+    // Create Intersection Observer
+    const observer = new IntersectionObserver(callback, options);
+  
+    const projectsSection = document.querySelector('#projects');
+    observer.observe(projectsSection);
+  });
+  
