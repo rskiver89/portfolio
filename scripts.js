8b14491cf978022e67b65fb6758928c1cfc8116f
rskiver89 <108497515+rskiver89@users.noreply.github.com>
2023-05-05 18:26:40 -0400

    update

--- scripts.js
+++ scripts.js
@@ -9,43 +9,22 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
   });
   
-// Project Scroll Animation
-document.addEventListener('DOMContentLoaded', function () {
-    const projectItems = document.querySelectorAll('.project-list li');
-  
-    // Function to handle the animation
-    const animateProjects = () => {
-      anime({
-        targets: projectItems,
-        opacity: [0, 1],
-        translateY: [-50, 0],
-        delay: anime.stagger(100),
-        easing: 'easeOutQuad',
-        duration: 1000
-      });
-    };
-  
-    // Intersection Observer Options
-    const options = {
-      root: null,
-      rootMargin: '0px',
-      threshold: 0.25
-    };
-  
-    // Intersection Observer Callback
-    const callback = (entries, observer) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          animateProjects();
-          observer.unobserve(entry.target); // Unobserve the target after the animation has been triggered
-        }
-      });
-    };
-  
-    // Create Intersection Observer
-    const observer = new IntersectionObserver(callback, options);
-  
-    const projectsSection = document.querySelector('#projects');
-    observer.observe(projectsSection);
-  });
-  
+  // Logo scroll
+const nameLogo = document.querySelector('#name-logo');
+const header = document.querySelector('header');
+const nameContainer = document.querySelector('#home h1');
+const headerHeight = header.offsetHeight;
+
+window.addEventListener('scroll', () => {
+  const nameRect = nameContainer.getBoundingClientRect();
+
+  if (nameRect.top + nameRect.height <= headerHeight) {
+    nameLogo.style.opacity = '1';
+  } else {
+    nameLogo.style.opacity = '0';
+  }
+});
+
+
+
+
