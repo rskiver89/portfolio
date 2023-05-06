947c635c5870de6930d8551cbeb1cc7283547e8f
rskiver89 <108497515+rskiver89@users.noreply.github.com>
2023-05-06 15:12:47 -0400

    update project styling

--- scripts.js
+++ scripts.js
@@ -8,6 +8,27 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
       });
     });
   });
+
+  const navLinks = document.querySelectorAll('nav ul li a');
+const sections = document.querySelectorAll('section');
+
+const options = {
+  rootMargin: '-50% 0px -50% 0px',
+  threshold: 0
+};
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    const index = Array.from(sections).indexOf(entry.target);
+    if (entry.isIntersecting) {
+      navLinks.forEach(link => link.classList.remove('active'));
+      navLinks[index].classList.add('active');
+    }
+  });
+}, options);
+
+sections.forEach(section => observer.observe(section));
+
   
   // Logo scroll
 const nameLogo = document.querySelector('#name-logo');
@@ -23,6 +44,41 @@ window.addEventListener('scroll', () => {
   } else {
     nameLogo.style.opacity = '0';
   }
+});
+
+// Contact Modal
+document.addEventListener("DOMContentLoaded", function () {
+  var modal = document.getElementById("contact-modal");
+  var btn = document.getElementById("contact-btn");
+  var span = document.getElementsByClassName("close")[0];
+
+  btn.onclick = function () {
+    modal.style.display = "block";
+  };
+
+  span.onclick = function () {
+    modal.style.display = "none";
+  };
+
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  };
+});
+
+const projects = document.querySelectorAll('.project');
+const projectInfos = document.querySelectorAll('.project-info h2');
+
+projects.forEach((project, index) => {
+  project.addEventListener('mouseover', () => {
+    projectInfos[index].style.color = '#005ea8';
+
+  });
+
+  project.addEventListener('mouseout', () => {
+    projectInfos[index].style.color = 'white';
+  });
 });
 
 
